e2f89505aef6a4b15cc793ee9c39e35cb7969d97
vijay <vijayakumar-palan@hcl.com>
2020-09-04 17:03:11 +0530

    updated the form with formik validation for create user.

--- src/component/createUser.js
+++ src/component/createUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { updateUser, createUser } from '../service/userService'
-import { Form,Button } from 'react-bootstrap';
+import { createUser } from '../service/userService';
+import { Formik } from 'formik'
+import * as Yup from 'yup';
 
 
 export class CreateUser extends Component {
@@ -13,13 +14,27 @@ export class CreateUser extends Component {
         }
     }
 
+    validationSchema = Yup.object({
+        FName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .min(3, 'Must be 3 characters or more')
+            .required('Firstname is Required'),
+        LName: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .min(3, 'Must be 3 characters or more')
+            .required('Lastname is Required'),
+        UName: Yup.string()
+            .required('Username is Required')
+            .min(3, 'Must be 3 characters or more')
+            .max(15, 'Must be 15 characters or less')
+    })
+
     componentDidMount() {
     }
 
     render() {
 
         const handlesumbit = () => {
-
 
             createUser(this.state.userdetils)
                 .then(response => {
@@ -40,25 +55,54 @@ export class CreateUser extends Component {
 
         return (
             <div className="col-md-8 margin-100">
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="First Name" onChange={(e) => handlechange(e, 'FName')} value={this.state.userdetils.FName} />
-                </Form.Group>
+                <Formik
+                    initialValues={{ FName: '', LName: '', UName: '' }}
+                    validationSchema={this.validationSchema}>
+                    {(props) => (
+                        <form onSubmit={props.handleSubmit}>
+                            <div className="form-group">
+                                <label for="FName">First Name</label>
+                                <input
+                                type="text"
+                                name="FName"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.FName}
+                            />
+                            <small id="emailHelp" className="form-text error">{props.errors.FName && props.touched.FName && props.errors.FName}</small>
+                            </div>
+                          
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Last Name" onChange={(e) => handlechange(e, 'LName')} value={this.state.userdetils.LName} />
-                </Form.Group>
+                            <div className="form-group">
+                                <label for="LName">Last Name</label>
+                                <input
+                                type="text"
+                                name="LName"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.LName}
+                            />
+                            <small id="emailHelp" className="form-text error">{props.errors.LName && props.touched.LName && props.errors.LName}</small>
+                            </div>
 
-                <Form.Group controlId="formUserName">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" onChange={(e) => handlechange(e, 'UName')} value={this.state.userdetils.UName} />
-                </Form.Group>
-
-                <Button variant="primary" type="button" onClick={(e) => handlesumbit()} >
-                    Save</Button>
-            </Form>
+                            <div className="form-group">
+                                <label for="UName">User Name</label>
+                                <input
+                                type="text"
+                                name="UName"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.UName}
+                            />
+                            <small id="emailHelp" className="form-text error"> {props.errors.UName && props.touched.UName && props.errors.UName}</small>
+                            </div>
+                            <button type="submit" disabled={props.isSubmitting} className="btn btn-primary">
+                                Submit
+                         </button>
+                            <pre>{JSON.stringify(props.values)}</pre>
+                        </form>
+                    )}
+                </Formik>
             </div>
         )
     }
